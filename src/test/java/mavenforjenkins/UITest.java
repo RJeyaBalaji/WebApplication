8e0dad7672679a01867d73ff7a8e7b013de54ae6
Jeya Balaji R <48553598+RJeyaBalaji@users.noreply.github.com>
2023-06-15 18:18:11 +0530

    Update UITest.java

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.testng.annotations.Optional;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UITest 
