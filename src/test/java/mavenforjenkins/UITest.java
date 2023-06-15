4c2bfaa67de406da4f54b640389617850b31e333
Jeya Balaji R <48553598+RJeyaBalaji@users.noreply.github.com>
2023-06-15 18:22:21 +0530

    Update UITest.java

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -16,30 +16,35 @@ public class UITest
 	
 	@Test
 	@Parameters("Browser")
-	public void startBrowser(@Optional("chrome") String browserName)
+	public void startBrowser(@Optional("chrome") String browserName) {
+  	System.out.println("Parameter value is " + browserName);
+   	WebDriver driver = null;
+
+   	if (browserName != null && browserName.contains("Chrome")) {
+        	WebDriverManager.chromedriver().setup();
+        	ChromeOptions opt = new ChromeOptions();
+        	opt.addArguments("--headless");
+        	opt.addArguments("--no-sandbox");
+        	opt.addArguments("--disable-dev-shm-usage");
+        	driver = new ChromeDriver(opt);
+    	} 
+	else if (browserName != null && browserName.contains("Edge")) {
+        	WebDriverManager.edgedriver().setup();
+        	driver = new EdgeDriver();
+    	}
+
+    	if (driver != null) {
+        	driver.manage().window().maximize();
+        	driver.get("https://opensource-demo.orangehrmlive.com/");
+        	Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+        	driver.quit();
+    	} 
+	else 
 	{
-		System.out.println("Parameter value is "+browserName);
-		WebDriver driver=null;
-		
-		if(browserName.contains("Chrome"))
-		{
-			WebDriverManager.chromedriver().setup();
-			ChromeOptions opt=new ChromeOptions();
-			opt.addArguments("--headless");
-			opt.addArguments("--no-sandbox");
-			opt.addArguments("--disable-dev-shm-usage");
-			driver=new ChromeDriver(opt);
-		}
-		else if(browserName.contains("Edge"))
-		{
-			WebDriverManager.edgedriver().setup();
-			 driver=new EdgeDriver();
-		}
-		driver.manage().window().maximize();
-		driver.get("https://opensource-demo.orangehrmlive.com/");
-		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
-		driver.quit();
-	}
+        	System.out.println("Invalid browser name or browser is null");
+    }
+}
+
 	
 	
 }
